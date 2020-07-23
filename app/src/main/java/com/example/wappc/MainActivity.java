5970e097120a0d22e6220301b7642ee8d781ce88
Avinash <ananthulaavinashgoud@gmail.com>
2020-07-23 19:36:14 +0530

    Autologin done!!

--- app/src/main/java/com/example/wappc/MainActivity.java
+++ app/src/main/java/com/example/wappc/MainActivity.java
@@ -1,14 +1,96 @@
 package com.example.wappc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.wappc.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CircleImageView profile_pic;
+    TextView username;
+    Toolbar toolbar;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+       profile_pic=findViewById(R.id.profilepic);
+       username=findViewById(R.id.username);
+       reference= FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot snapshot) {
+               User user=snapshot.getValue(User.class);
+               username.setText(user.getUsername());
+
+               if(user.getImageURL().equals("default")){
+                   profile_pic.setImageResource(R.mipmap.ic_launcher);
+               }
+               else
+               {
+                  Glide.with(MainActivity.this).load(user.getImageURL()).into(profile_pic);
+               }
+
+           }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+       });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.logout:
+
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this,StartActivity.class));
+                finish();
+                return true;
+
+        }
+
+        return false;
     }
 }
