5fc82552c6e3b15740e17f4d7a2bc5d78e5328c7
Avinash <ananthulaavinashgoud@gmail.com>
2020-07-26 08:13:58 +0530

    Working Chat , User and Profile Fragments with chat feature and Uploading profile photo feature..!!

--- app/src/main/java/com/example/wappc/MainActivity.java
+++ app/src/main/java/com/example/wappc/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.wappc;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +16,11 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.wappc.Fragments.ChatsFragment;
+import com.example.wappc.Fragments.ProfileFragment;
+import com.example.wappc.Fragments.UsersFragment;
 import com.example.wappc.Model.User;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +28,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -39,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
-       profile_pic=findViewById(R.id.profilepic);
+       profile_pic=findViewById(R.id.profile_image);
        username=findViewById(R.id.username);
        reference= FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
@@ -70,6 +81,19 @@ public class MainActivity extends AppCompatActivity {
 
            }
        });
+
+        TabLayout tabLayout=findViewById(R.id.tab_layout);
+        ViewPager viewPager=findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter= new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"Users");
+        viewPagerAdapter.addFragment(new ProfileFragment(),"Profile");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -92,5 +116,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return false;
+    }
+
+    public static class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        public ViewPagerAdapter(FragmentManager fm){
+
+            super(fm,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+            this.fragments=new ArrayList<>();
+            this.titles=new ArrayList<>();
+
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+           return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment,String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }
